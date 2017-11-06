64f0fbde024500cd8981e8e562be743757633099
Ido Ben-Yair <ido.benyair@gmail.com>
2017-11-06 19:52:33 +0200

    Hello, kernel!

--- hr_device.c
+++ hr_device.c
@@ -9,3 +9,16 @@ MODULE_AUTHOR("KernelTLV");
 MODULE_DESCRIPTION("An imaginary heart rate monitor");
 MODULE_VERSION("0.1");
 
+static int __init hr_device_init(void)
+{
+    printk(KERN_INFO "Heart rate monitor driver loaded!\n");
+    return 0;
+}
+
+static void __exit hr_device_exit(void)
+{
+    printk(KERN_INFO "Heart rate monitor driver unloaded!\n");
+}
+
+module_init(hr_device_init);
+module_exit(hr_device_exit);
